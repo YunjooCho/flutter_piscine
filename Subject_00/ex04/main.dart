3e77ba41d95850780b6851d2369d993a2f9494de
YunjooCho <rollcake0317@naver.com>
2022-08-19 02:48:51 +0900

    2022/08/19

---
+++ Subject_00/ex04/main.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+ class MyApp extends StatelessWidget {
+   const MyApp({Key? key}) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+     return const MaterialApp(
+       title: 'Character Card',
+       home: PocketmonDetails(),
+     );
+   }
+//
+//   // _buildAppBar() { //★반환타입??
+//   //   return AppBar(
+//   //     title: Text('My Poket',
+//   //       style: TextStyle(
+//   //         fontWeight: FontWeight.bold,
+//   //       ),
+//   //     ),
+//   //     backgroundColor: Colors.yellow,
+//   //     elevation: 0.0, //AppBar가 떠있는 듯한 효과를 없앰, 높이라는 뜻으로 double형의 숫자로 수치 조절
+//   //   );
+//   // }
+//   //
+//   // PocketmonDetails _builBody(){
+//   //   return PocketmonDetails(
+//   //     imageURL:
+//   //       'https://mblogthumb-phinf.pstatic.net/MjAxNzAyMjVfMjMg/MDAxNDg3OTUzMTI3Mzc0._tG2RA_tY9IZcrw10kWz3YfLkhcuSRxm_rUQoLRhsQEg.hndrmcX4b8HI5c_EJB_JfftjG6C79zJXLQ0g6dZy9FQg.GIF.doghter4our/IMG_3900.GIF?type=w800'
+//   //   );
+//   // }
+//   //
+//   // @override
+//   // Widget build(BuildContext context) {
+//   //   return Scaffold(
+//   //     appBar: _buildAppBar(),
+//   //     backgroundColor: Colors.yellow[200],
+//   //     body: _builBody(),
+//   //   );
+//   // }
+}
+
+class PocketmonDetails extends StatelessWidget {
+  const PocketmonDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text('My Poket',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.yellow,
+        elevation: 0.0, //AppBar가 떠있는 듯한 효과를 없앰, 높이라는 뜻으로 double형의 숫자로 수치 조절
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(40.0, 30.0, 0.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, //두 개의 텍스트 시작점을 맞춰서 정렬
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: Image.network('https://mblogthumb-phinf.pstatic.net/MjAxNzAyMjVfMjMg/MDAxNDg3OTUzMTI3Mzc0._tG2RA_tY9IZcrw10kWz3YfLkhcuSRxm_rUQoLRhsQEg.hndrmcX4b8HI5c_EJB_JfftjG6C79zJXLQ0g6dZy9FQg.GIF.doghter4our/IMG_3900.GIF?type=w800').image,
+                radius: 100.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
+            Text('Name',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 3.0,
+            ),
+            Text('Pikachu',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0, //글자 간격
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 3.0,
+            ),
+            Text('Level',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text('Lv.4',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0, //글자 간격
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('Body Blow'),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('Eletric Shocks'),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('Electro Ball'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
