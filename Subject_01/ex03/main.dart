6beeea1b1d05f7e1e61b5622b7c05b9d8fb6b628
YunjooCho <rollcake0317@naver.com>
2022-08-24 21:29:01 +0900

    2022/08/24 update4

---
+++ Subject_01/ex03/main.dart
@@ -0,0 +1,128 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //scaffold제거(_RandomWordsState에서 설정)
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: RandomWords(), // REMOVE Scaffold
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
+  List<String> _subtitleContents = ['done', 'do not yet'];
+  //final idx = Random().nextInt(2);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ex02'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          final index = i;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          final idx = Random().nextInt(2);
+          final _displayContents = _subtitleContents[idx];
+          final alreadySaved = _saved.contains(_suggestions[index]);
+          
+          return Card(
+            child: ListTile(
+            title: Text(
+                _suggestions[index].asPascalCase,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                )
+            ),
+            subtitle: Text(_subtitleContents[idx]),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            },
+          ),
+            elevation: 2.0,
+          );
+        },
+      ),
+    );
+  }
+
+  void _pushSaved() { //Navigator스택에 즐겨찾기한 문장을 push(), pop()은 별도 구현필요없음
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+                (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+}
